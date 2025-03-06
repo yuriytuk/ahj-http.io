ed30026aa6ab567971a635f569ad29573e2de28d
Yuriy <yuriytukal@yandex.ru>
2025-03-06 15:19:52 +0300

    изменения

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,7 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import jest from "eslint-plugin-jest";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
+
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
-  },
-  pluginJs.configs.recommended,
-  eslintPluginPrettierRecommended,
-  {
-    rules: {
-      "no-unused-vars": "warn",
-    },
-  },
-  {
-    ignores: ["dist/*"],
-  },
-  {
-    files: ["**/*.test.js"],
-    ...jest.configs["flat/recommended"],
-    rules: {
-      ...jest.configs["flat/recommended"].rules,
-      "jest/prefer-expect-assertions": "off",
-    },
-  },
+  {languageOptions: { globals: globals.browser }},
 ];
